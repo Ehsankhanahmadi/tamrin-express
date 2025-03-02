c88490fc4687c2d6d57fa1bb9c24104f455c317e
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-03-02 17:18:12 +0330

    tamrin routing and params in url in express

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-const express = require("express")
-const app = express()
-const PORT = 2025
+const express = require("express");
+const app = express();
+const PORT = 2025;
 
-// express khode ststuse code hara ersal mikonad 
+// express khode ststuse code hara ersal mikonad
 // 404 khodcar darad
 // content-type ha ra khodash tashkhis midahad
 // keili az carhara rahat mikonad
-app.get('/',(req , res) => {
-    res.send("hi express")
+
+let data = [
+  {
+    id: 0,
+    name: "ehsan",
+    lastname: "khan",
+  },
+  {
+    id: 1,
+    name: "ehsan",
+    lastname: "khan",
+  },
+  {
+    id: 2,
+    name: "ehsan",
+    lastname: "khan",
+  },
+  {
+    id: 3,
+    name: "ehsan",
+    lastname: "khan",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("hi express");
+});
+
+app.get('/data', (req , res) => {
+    res.send(data)
 })
 
-app.listen(PORT,()=>{
-    console.log(`
-        http://127.0.0.1:${PORT}
-        `)
+app.get('/data/:ID', (req , res) => {
+    const id = req.params.ID
+    if(data[id])
+        res.send(data[id])
+    else
+        res.sendStatus(404)
 })
+
+app.listen(PORT, () => {
+  console.log(`
+        http://127.0.0.1:${PORT}
+        `);
+});
