2b554144c4280719c4667aa101356a0ae8d8c73c
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-03-03 21:20:23 +0330

    tamrin layout in hbs templeate engine

--- controllers/img-controller.js
+++ controllers/img-controller.js
@@ -2,6 +2,10 @@ const path = require("node:path")
 
 function img(req , res){
     res.sendFile(path.join(__dirname,'..','public',"nature.jpg"))    
+    // baraieh tamrin ast
+    // res.render("test",{
+    //     test:"this is a test text"
+    // })
 }
 
 module.exports = {img}
